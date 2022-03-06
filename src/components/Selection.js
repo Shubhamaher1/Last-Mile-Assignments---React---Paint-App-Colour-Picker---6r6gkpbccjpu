788b7dd804ee45dcb8ce64b809e90fbaaee211b3
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-03-06 18:10:01 +0530

    h

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,10 @@
+import React, { useState } from "react";
 
+export default function Selection({ applyColor }) {
+  let [color, setColor] = useState({ background: "" });
+  return (
+    <div className="fix-box" style={color} onClick={() => applyColor(setColor)}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
